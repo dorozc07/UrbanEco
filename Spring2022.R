97925d14fc4f0434fe9d1bea6fa20ab56af047a8
Dina Orozco <dinaorozco@Dinas-MacBook-Pro.local>
2022-02-06 21:01:44 -0600

    RSQA data sheet

--- Spring2022.R
+++ Spring2022.R
@@ -1,3 +1,14 @@
-Hi 
-#This is a test
-#Here is the second test here we go 
+library(plyr)
+library(dplyr)
+library(ggbiplot)
+library(readr)
+library(tidyr)
+
+results <- read.csv("Results.csv")
+sites <- read.csv("Sites.csv")
+median_results <- results %>%
+  group_by(PARM_NM, SITE_NO) %>%
+  summarize(median = median(RESULT_VA))
+short <- pivot_wider(median_results, id_cols = SITE_NO, names_from = PARM_NM, values_from = median) 
+#this sees how many NA's there are 
+colSums(is.na(short))
