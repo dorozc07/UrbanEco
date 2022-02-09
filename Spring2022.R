2a7c14803667e09bcb7991ffddb313114acb9357
Dina Orozco <dinaorozco@Dinas-MacBook-Pro.local>
2022-02-09 16:04:44 -0600

    Help

--- Spring2022.R
+++ Spring2022.R
@@ -9,6 +9,11 @@ sites <- read.csv("Sites.csv")
 median_results <- results %>%
   group_by(PARM_NM, SITE_NO) %>%
   summarize(median = median(RESULT_VA))
-short <- pivot_wider(median_results, id_cols = SITE_NO, names_from = PARM_NM, values_from = median) 
+Short_data <- pivot_wider(median_results, id_cols = SITE_NO, names_from = PARM_NM, values_from = median) 
 #this sees how many NA's there are 
 colSums(is.na(short))
+#Selecting columns without NA's 
+Interestingcolumns_data<-as.data.frame(Short_data[,c(2,7,8,9,11,12,16,17)])
+rownames(Interestingcolumns_data)<-Short_data$SITE_NO
+#Removing rows (sites) with NA's 
+finaldata <- Interestingcolumns_data[complete.cases(Interestingcolumns_data), ]
